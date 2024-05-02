d8591c9432efa3e31f84174a8c598aa5aa5b02eb
giapgiap <hoangkhacgiap1108@gmail.com>
2024-05-02 16:42:59 +0700

    config sercurity

--- src/main/java/com/example/util/init.java
+++ src/main/java/com/example/util/init.java
@@ -1,4 +1,11 @@
 package com.example.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
 public class init {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 }
